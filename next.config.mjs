d7d47bcfc9c5d0c21e88c679d020163430019956
mohammed shibil <mohammedshibil@atts.in>
2025-01-03 19:39:32 +0530

    blog fixed

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output: 'export',
   images: {
     domains: ['flowbite.s3.amazonaws.com'],
+    unoptimized: true
     // If you have other domains, add them to this array
   },
     reactStrictMode: true,
